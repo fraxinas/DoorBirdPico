68815f09f0f755142d4a4ad7ea9693f00633a926
Andreas Frisch <fraxinas@purplegecko.de>
2021-05-21 21:45:23 +0200

    Software: Add relay input and outputs

--- software/src/doorbirdpico.c
+++ software/src/doorbirdpico.c
@@ -29,6 +29,25 @@
 #define PIN_C_GREEN 20
 #define PIN_C_RED 19
 
+#define PIN_RELAY_OUT 28
+
+#define PIN_RELAY_IN1 27
+#define PIN_RELAY_IN2 26
+
+typedef enum {
+  DOOR_UNKNOWN,
+  DOOR_CLOSED,
+  DOOR_OPEN
+} door_state_t;
+
+typedef enum {
+  LOCK_UNKNOWN,
+  LOCK_LOCKED,
+  LOCK_UNLOCKED
+} lock_state_t;
+
+door_state_t door_state;
+lock_state_t lock_state;
 pwm_config pwm_conf;
 int fade_pins[2] = {-1, -1};
 
@@ -102,12 +121,13 @@ void key_callback(uint gpio, uint32_t events)
           case PIN_A_KEY:
               irq_pin = PIN_A_GREEN;
               uart_puts(uart0, "101#\r\n");
+              printf("Key B rang\r\n");
               i = 0;
               break;
           case PIN_B_KEY:
               irq_pin = PIN_B_BLUE;
               uart_puts(uart0, "102#\r\n");
-              printf("sent 102#\\r\\n");
+              printf("Key B rang\r\n");
               i = 1;
               break;
         }
@@ -136,12 +156,83 @@ void key_callback(uint gpio, uint32_t events)
     }
 }
 
+void set_key_c_color()
+{
+    if (lock_state == LOCK_LOCKED) {
+      gpio_put(PIN_C_RED, 1);
+      gpio_put(PIN_C_GREEN, 0);
+    } else {
+      gpio_put(PIN_C_GREEN, 1);
+      sleep_ms(1000);
+      gpio_put(PIN_C_RED, 0);
+    }
+}
+
+void sensor_input(uint gpio, uint32_t events)
+{
+    gpio_event_string(event_str, events);
+    printf("Sensor input port %d %s", gpio, event_str);
+     
+    bool rise = (events & 0x8);
+    
+    if (events & 0x8)
+    {
+        // Load the configuration into our PWM slice, and set it running.
+        int i, irq_pin;
+
+        switch (gpio)
+        {
+          case PIN_C_KEY:
+              uart_puts(uart0, "113#\r\n");
+              printf("Inside Button pressed\r\n");
+              break;
+          case PIN_REED_POST:
+              if (!rise) {
+                uart_puts(uart0, "114#\r\n");
+                printf("Letterbox opened\r\n");
+              }
+              break;
+          case PIN_REED_DOOR:
+              door_state = rise ? DOOR_CLOSED : DOOR_OPEN;
+              printf("Reed toggle -> door_state = %s\r\n", door_state == DOOR_OPEN ? "OPENED" : "CLOSED");
+              break;
+          case PIN_RELAY_IN1:
+              if (!rise) {
+                printf("Relay 1 falling -> Buzzer off!\r\n");
+                gpio_put(PIN_RELAY_OUT, 0);
+              } else { 
+                if (lock_state == LOCK_UNLOCKED) {
+                  printf("Relay 1 rising -> Buzzer on!\r\n");
+                  gpio_put(PIN_RELAY_OUT, 1);
+                } else {
+                  printf("Relay 1 rising but door is locked, waiting for unlock...\r\n");
+                }
+              }
+              break;
+          case PIN_RELAY_IN2:
+              lock_state = rise ? LOCK_LOCKED : LOCK_UNLOCKED;
+              printf("Relay 2 toggle -> lock_state = %s\r\n", lock_state == LOCK_LOCKED ? "LOCKED" : "UNLOCKED");
+              set_key_c_color();
+              if (lock_state == LOCK_UNLOCKED && gpio_get (PIN_RELAY_IN1)) {
+                printf("Door got unlocked & Relay 1 still on -> Buzzer on!\r\n");
+                gpio_put(PIN_RELAY_OUT, 1);
+              }
+              break;
+        }
+    }
+}
+
 int setup_irq()
 {
     stdio_init_all();
     printf("Hello GPIO IRQ\n");
     gpio_set_irq_enabled_with_callback(PIN_A_KEY, GPIO_IRQ_EDGE_RISE, true, &key_callback);
     gpio_set_irq_enabled_with_callback(PIN_B_KEY, GPIO_IRQ_EDGE_RISE, true, &key_callback);
+    gpio_set_irq_enabled_with_callback(PIN_C_KEY, GPIO_IRQ_EDGE_RISE, true, &sensor_input);
+    gpio_set_irq_enabled_with_callback(PIN_REED_POST, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true, &sensor_input);
+    gpio_set_irq_enabled_with_callback(PIN_REED_DOOR, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true, &sensor_input);
+    gpio_set_irq_enabled_with_callback(PIN_RELAY_IN1, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true, &sensor_input);
+    gpio_set_irq_enabled_with_callback(PIN_RELAY_IN2, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true, &sensor_input);
 }
 
 void on_pwm_wrap()
@@ -206,6 +297,16 @@ void on_pwm_wrap()
     }
 }
 
+int setup_gpio() {
+    gpio_set_dir(PIN_RELAY_OUT, GPIO_OUT);
+    gpio_set_dir(PIN_C_BLUE, GPIO_OUT);
+    gpio_set_dir(PIN_C_GREEN, GPIO_OUT);
+    gpio_set_dir(PIN_C_RED, GPIO_OUT);
+    gpio_put(PIN_RELAY_OUT, 0);
+    door_state = DOOR_UNKNOWN;
+    lock_state = LOCK_UNKNOWN;
+}
+
 int setup_pwm()
 {
     // Tell the LED pin that the PWM is in charge of its value.
@@ -264,6 +365,7 @@ int setup_uart()
 
 int main()
 {
+    setup_gpio();
     setup_irq();
     setup_pwm();
     setup_uart();
